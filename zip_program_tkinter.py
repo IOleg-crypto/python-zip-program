5ca6ba3b60659233a25a4674a27dc9e7a933be99
IOleg-sus <guchkooleg7@gmail.com>
2024-05-10 16:46:06 +0300

    added gitignore

--- zip_program_tkinter.py
+++ zip_program_tkinter.py
@@ -1,17 +1,31 @@
 import os
 import zipfile
+import shutil
 from tkinter import *
 from tkinter import filedialog
+from tkinter.messagebox import showerror, showwarning, showinfo
+from tkinter import messagebox as mb
  
-def create_zip(directory_to_zip, zip_file_name):
-    with zipfile.ZipFile(zip_file_name, 'w') as zipf:
-        for root, dirs, files in os.walk(directory_to_zip):
-            for file in files:
-                zipf.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), directory_to_zip))
-               
-def extract_zip(zip_file_name):
-    pass
+def create_zip(directory_to_zip : str, zip_file_name : str):
+    shutil.make_archive(zip_file_name, 'zip', directory_to_zip)
+    mb.showinfo("Success", "Archive file created successfully.")
+    mb.askyesno(title="Вопрос",
+                         message="Show file?")
+    if mb.askyesno:
+       os.system("C:\Windows\explorer.exe")
+    else:
+       pass
     
+               
+def extract_zip(directory_to_zip : str):
+    if shutil.unpack_archive(directory_to_zip):
+        mb.showinfo("Success", "Archive file unpacked successfully.")  
+    else:
+        mb.showerror("Error", "Archive file unpacking failed.")
+   
+def button_pressed():
+    print("Button pressed")
+  
 def main():
     #parent window
     main_window = Tk()
@@ -26,18 +40,11 @@ def main():
     button_extractZip = Button(main_window , text = "Extract zip" , command = extract_zip)
     button_extractZip.pack()
     button_extractZip.place(x = 20 , y = 300 , width = 100 , height = 30)
-    #textbox
-    """""
-    textbox = Text(main_window , height = 1 , width = 20 , font="Arial" )
-    textbox.place(x = 50 , y = 120)
-    textbox.pack()
-    """""
     #filepath(dialogs)
-    
+    #button_createZip.bind("<Button-1>", lambda event: button_pressed())
     if button_createZip:
-       filedialog.askdirectory(title="Select Directory to Pack")
        filepath = filedialog.askdirectory(title="Select Directory to Pack")
-       create_zip(filepath , "test.zip")
+       create_zip(filepath , "test")
     if button_extractZip:
        filepath = filedialog.askdirectory(title="Select Directory to Extract")
        extract_zip(filepath)
