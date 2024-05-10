15349e69b8cedac710630f8f5c2b73981e72bf4c
IOleg-sus <guchkooleg7@gmail.com>
2024-05-10 16:02:42 +0300

    added project

--- zip_program_tkinter.py
+++ zip_program_tkinter.py
@@ -3,9 +3,6 @@ import zipfile
 from tkinter import *
 from tkinter import filedialog
  
-def clear():
-    pass
-
 def create_zip(directory_to_zip, zip_file_name):
     with zipfile.ZipFile(zip_file_name, 'w') as zipf:
         for root, dirs, files in os.walk(directory_to_zip):
@@ -20,7 +17,7 @@ def main():
     main_window = Tk()
     main_window.title("Zip packer && extractor")
     main_window.call('tk' , 'scaling' , 1.5)
-    main_window.geometry("640x480")
+    main_window.geometry("500x350")
     #main functionality
     button_createZip = Button(main_window, text="Create ZIP", command=create_zip)
     button_createZip.pack()
@@ -30,21 +27,22 @@ def main():
     button_extractZip.pack()
     button_extractZip.place(x = 20 , y = 300 , width = 100 , height = 30)
     #textbox
+    """""
     textbox = Text(main_window , height = 1 , width = 20 , font="Arial" )
     textbox.place(x = 50 , y = 120)
     textbox.pack()
+    """""
     #filepath(dialogs)
+    
     if button_createZip:
+       filedialog.askdirectory(title="Select Directory to Pack")
        filepath = filedialog.askdirectory(title="Select Directory to Pack")
-       fileName = Label(main_window , text =  "Name : ")
-       fileName.place(x = 50 , y = 120)
        create_zip(filepath , "test.zip")
     if button_extractZip:
        filepath = filedialog.askdirectory(title="Select Directory to Extract")
-       fileName = Text(main_window , height = 1 , width = 20 , font="Arial" )
-       fileName.place(x = 50 , y = 120)
+       extract_zip(filepath)
     
-    print("Selected file: ", filepath)
+    # print("Selected file: ", filepath)
 
 #main window
 if __name__ == "__main__":
