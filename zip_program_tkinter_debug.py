da80d6eb35689506b137470fd92225f387fc4c44
IOleg-sus <guchkooleg7@gmail.com>
2024-05-13 10:41:41 +0300

    Added compression type

--- zip_program_tkinter_debug.py
+++ zip_program_tkinter_debug.py
@@ -4,28 +4,41 @@ from tkinter import *
 from tkinter import filedialog
 from tkinter.messagebox import showerror, showwarning, showinfo
 from tkinter import messagebox as mb
+from tkinter import simpledialog as sd
+from tkinter.simpledialog import askstring
 from zipfile import ZipFile
 
+#compression library
+import zlib
+import lzma
+import bz2
+
 def create_zip(zipFileName):
-   destination_directory = filedialog.askdirectory(title="Choose directory to save zip")
+   destination_directory = filedialog.askdirectory(title="Choose directory to save *.format")
    if not destination_directory:
         mb.showinfo("Info","No directory selected.")
         return
 
     # Ask the user to choose files to include in the zip file
-   filenames = filedialog.askopenfilenames(title="Choose files to include in zip")
+   filenames = filedialog.askopenfilenames(title="Choose files to include in *.format")
    if not filenames:
         mb.showinfo("Info","No files selected.")
         return
-
+        
+   answer = sd.askstring("Input", "Choose compression type(zipfile.ZIP_DEFLATED , zipfile.ZIP_BZIP2 , zipfile.ZIP_LZMA)", parent=None)
+   if not answer:
+      mb.showinfo("Info", "No compression type selected.")
+   if answer:
+      mb.showinfo("Info", f"Compression type: {answer}")
+                                
     # Create the zip file in the chosen directory
    zip_name = os.path.join(destination_directory, zipFileName)
-   with zipfile.ZipFile(zip_name, mode="w") as archive:
+   with zipfile.ZipFile(zip_name, mode="w" , compression=zipfile.ZIP_DEFLATED) as archive:
         for filename in filenames:
             arcname = os.path.relpath(filename, destination_directory)
             arcname = arcname.replace(os.path.sep, '/')  # Replace backslashes with forward slashes
             archive.write(filename, arcname=arcname)
-   mb.showinfo("Info", f"Zip file '{zip_name}' created successfully.")
+   mb.showinfo("Info", f"File '{zip_name}' created successfully.")
 
                
 def extract_zip():
@@ -43,14 +56,14 @@ def extract_zip():
     with zipfile.ZipFile(file_name, 'r') as zip_extract:
         zip_extract.extractall(directory)
         
-    mb.showinfo("Success", "Zip file extracted to: " + directory)
+    mb.showinfo("Success", "File extracted to: " + directory)
 
 
 def main():
     #parent window
     main_window = Tk()
     main_window.title("File archiver")
-    main_window.call('tk' , 'scaling' , 1.5)
+    main_window.call('tk' , 'scaling' , 2)
     main_window.geometry("500x150")
     #main functionality
     button_createZip_launch = Button(main_window, text="Create ZIP")
@@ -64,12 +77,20 @@ def main():
     button_extractZip.bind("<Button-1>", lambda event: extract_zip())
     textbox = Text(main_window , width= 40 , height = 1)
     textbox.pack()
-    textbox.place(x = 150 , y = 50, width = 300 , height = 30)
+    textbox.place(x = 150 , y = 30, width = 300 , height = 30)
     #label
-    labelText = Label(main_window , text = "Enter the name for zip file(*.format)")
+    labelText = Label(main_window , text = "Enter the name for(*.format)" , font= "Arial 10")
+    labelText.place(x = 150 , y = 100 , width = 500 , height = 30)
     labelText.pack()
-  
-
+    
+    #spinbox(has compression)
+    """""
+    spinbox_var = StringVar()
+    compresssion_levels = ["ZIP_DEFLATED" , "ZIP_BZIP2" , "ZIP_LZMA"]
+    spinbox_compression = Spinbox(main_window , textvariable = spinbox_var , values = compresssion_levels , state = "readonly")
+    #spinbox_compression.place()
+    spinbox_compression.pack(anchor="s")
+    """""
 #main window
 if __name__ == "__main__":
     main()
